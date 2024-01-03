e5e175781016807c7c3c92892a1ebc716db9f187
Liam Spedding <liam.spedding@creode.co.uk>
2024-01-03 15:53:00 +0000

    Remove style and script registration code

--- src/LaravelNovaEventsNestedCategories.php
+++ src/LaravelNovaEventsNestedCategories.php
@@ -4,7 +4,6 @@ namespace Creode\LaravelNovaEventsNestedCategories;
 
 use Illuminate\Http\Request;
 use Laravel\Nova\Menu\MenuSection;
-use Laravel\Nova\Nova;
 use Laravel\Nova\Tool;
 
 class LaravelNovaEventsNestedCategories extends Tool
@@ -16,14 +15,12 @@ class LaravelNovaEventsNestedCategories extends Tool
      */
     public function boot()
     {
-        Nova::script('laravel-nova-events-nested-categories', __DIR__.'/../dist/js/tool.js');
-        Nova::style('laravel-nova-events-nested-categories', __DIR__.'/../dist/css/tool.css');
     }
 
     /**
      * Build the menu that renders the navigation links for the tool.
      *
-     * @param  \Illuminate\Http\Request $request
+     * @param \Illuminate\Http\Request $request
      * @return mixed
      */
     public function menu(Request $request)
