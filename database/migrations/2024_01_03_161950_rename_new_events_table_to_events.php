34a292a9e93348d3237f69fb1e3811be96977741
Liam Spedding <liam.spedding@creode.co.uk>
2024-01-04 11:06:04 +0000

    Add if statements around rename

--- database/migrations/2024_01_03_161950_rename_new_events_table_to_events.php
+++ database/migrations/2024_01_03_161950_rename_new_events_table_to_events.php
@@ -6,11 +6,16 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::rename('new_events', 'events');
+        if (Schema::hasTable('new_events')) {
+            Schema::rename('new_events', 'events');
+        }
+
     }
 
     public function down(): void
     {
-        Schema::rename('events', 'new_events');
+        if (Schema::hasTable('events')) {
+            Schema::rename('events', 'new_events');
+        }
     }
 };
